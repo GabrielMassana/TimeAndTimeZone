335a53fab2277d5dc2bf38b5c43b31e55a547cbb
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-04-23 09:33:15 +0100

    Initial Commit

---
+++ GMT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GMT
+//
+//  Created by Gabriel Massana on 23/04/2014.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
