1b76551c69f653f35814121fec712c04e3630f25
Gabriel Massana <Gabi@LOCK8s-Mac-mini.local>
2014-04-23 09:43:09 +0700

    first commit

--- GMT/ViewController.m
+++ GMT/ViewController.m
@@ -26,4 +26,45 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)option1:(id)sender
+{
+    NSDate *localDate = [NSDate date];
+    
+    NSTimeInterval secondsInFourHours = -4 * 60 * 60;
+    NSDate *dateThreeHoursAhead = [localDate dateByAddingTimeInterval:secondsInFourHours];
+    NSDateFormatter *dt = [[NSDateFormatter alloc] init];
+    [dt setDateFormat:@"h:mm a"];
+    
+    NSLog(@"GMT+7(-4) = %@", [dt stringFromDate:dateThreeHoursAhead]);
+}
+
+- (IBAction)option2:(id)sender
+{
+    NSDate *localDate = [NSDate date];
+    
+    NSTimeInterval secondsInThreeHours = 3 * 60 * 60;
+    NSDate *dateThreeHoursAhead = [localDate dateByAddingTimeInterval:secondsInThreeHours];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"GMT"];
+    [dateFormatter setTimeZone:timeZone];
+    [dateFormatter setDateFormat:@"h:mm a"];
+    NSString *date = [dateFormatter stringFromDate:dateThreeHoursAhead];
+    NSLog(@"GMT+3 = %@", date);
+
+}
+- (IBAction)option3:(id)sender
+{
+    NSDate *localDate = [NSDate date];
+
+    NSDateFormatter *dt = [[NSDateFormatter alloc] init];
+    [dt setDateFormat:@"h:mm a"];
+    NSTimeZone *timeZone = [NSTimeZone timeZoneWithName:@"EAT"];
+    [dt setTimeZone:timeZone];
+    NSLog(@"EAT = %@", [dt stringFromDate:localDate]);
+}
+
+
+
 @end
